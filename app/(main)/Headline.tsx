454801837a1eb6fbd162135d8a2b0129c1a3bf05
xiaobingtech <161550081+xiaobingtech@users.noreply.github.com>
2024-07-16 10:40:34 +0800

    fix first

--- app/(main)/Headline.tsx
+++ app/(main)/Headline.tsx
@@ -3,7 +3,7 @@
 import { motion } from 'framer-motion'
 import Balancer from 'react-wrap-balancer'
 
-import { SparkleIcon, UserSecurityIcon } from '~/assets'
+// import { SparkleIcon, UserSecurityIcon } from '~/assets'
 // import { PeekabooLink } from '~/components/links/PeekabooLink'
 import { SocialLink } from '~/components/links/SocialLink'
 
